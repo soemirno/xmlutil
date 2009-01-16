34c5b94ef7fded2e9913544f8deac49fe0ebc574
soemirno <soemirno@gmail.com>
2009-01-16 14:08:38 +0100

    Added simple test to remove duplicates from a list

--- src/test/scala/net/soemirno/xmlUtil/mergeDuplicatesSpecTest.scala
+++ src/test/scala/net/soemirno/xmlUtil/mergeDuplicatesSpecTest.scala
@@ -1,18 +1,34 @@
 package net.soemirno.xmlUtil
 
-
-import org.specs.runner.{ Runner, JUnit}
+import org.specs.runner.{Runner, JUnit}
 import org.specs.Specification
 
 class mergeDuplicatesSpecTest extends Runner(mergeDuplicatesSpec) with JUnit
 
-
 object mergeDuplicatesSpec extends Specification {
-  "'hello world' has 11 characters" in {
-      "hello world".size must_== 11
+
+  val listWithDuplicates = List(1, 2, 2, 3, 4)
+
+  def normalizeList(list: List[int]): List[int] =
+    {
+      if (list.length == 1)
+        list
+      else {
+        if (list.head == list.tail.head) {
+          list.head :: normalizeList(list.tail.tail)
+        } else {
+          list.head :: normalizeList(list.tail)
+        }
+      }
+    }
+
+  "normalized list contains 4 items" in {
+    (normalizeList(listWithDuplicates)).size must_== 4
   }
 
-  "'hello world' matches 'h.* w.*'" in {
-    "hello world" must beMatching("h.* w.*")
+  "normalized list has '3' as third element" in {
+    (normalizeList(listWithDuplicates))(2) must_== 3
   }
+
+
 }
